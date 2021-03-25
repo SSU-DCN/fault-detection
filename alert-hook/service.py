3daae90903f3dd60d04ebab9abb49cf59d3311b7
Hiep <maiquanghiepdl@gmail.com>
2021-03-25 01:51:18 +0000

    update alert hook

--- alert-hook/service.py
+++ alert-hook/service.py
@@ -8,16 +8,32 @@ def alertHandler(request):
      "X-Auth-Token": get_token(),
      "Content-Type": "application/json; charset=utf-8"
     }
-
     alerts = request.get("alerts", [])
-    for alert in alerts:
-      labels = alert.get("labels", {})
-      data = {
-        "alert": {
-          "event_id": labels.get("event_id")
-        }
-      }
+    if len(alerts):
+     for alert in alerts:
+       labels = alert.get("labels", {})
+       print(alert, labels)
+       data = {
+         "alert": {
+           "event_id": labels.get("event_id")
+         }
+       }
+    else:
+     labels = request.get("labels", {})   
+     data = { 
+      "alert": {
+         "event_id":  labels.get("event_id")
+         }
+       }
 
+    data['alert']['data'] = {}
+    data['alert']['data']['body'] =  {
+        "name": "test1",
+        "replicas": 1,
+        "action":"live-migration",
+        "sourcePod":"nginx-migration-16",
+        "destHost":"worker2"
+    }
     r = requests.post('http://127.0.0.1:9890/v1.0/alerts.json', data=json.dumps(data),headers=headers)
     
     return {}
