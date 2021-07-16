54066a8d0c923f60907e2eedd8b3608a747b64ec
Hiep <maiquanghiepdl@gmail.com>
2021-07-16 07:57:58 +0000

    update config

--- alert-hook/service.py
+++ alert-hook/service.py
@@ -2,13 +2,14 @@ from keystone import get_token
 import json
 import requests
 from datetime import datetime
-
+import os
 def alertHandler(request):
     headers = {
      "X-Auth-Token": get_token(),
      "Content-Type": "application/json; charset=utf-8"
     }
     alerts = request.get("alerts", [])
+    print(alerts)
     if len(alerts):
      for alert in alerts:
        labels = alert.get("labels", {})
@@ -21,20 +22,15 @@ def alertHandler(request):
      labels = request.get("labels", {})   
      data = { 
       "alert": {
-         "event_id":  labels.get("event_id")
+         "event_id":  labels.get("event_id"),
+         "data": {
+            "instance": labels.get("instance", "")
+          }
          }
-       }
-
-    data['alert']['data'] = {}
-    data['alert']['data']['instance'] = '192.168.5.192'
-    data['alert']['data']['body'] =  {
-        "name": "test" + str(datetime.now().timestamp()),
-        "replicas": 1,
-        "action":"live-migration",
-        "sourcePod":"video",
-        "destHost":"worker1"
-    }
-    r = requests.post('http://127.0.0.1:9890/v1.0/alerts.json', data=json.dumps(data),headers=headers)
+      }
+    
+    orchestration_url = os.environ.get('orchestration_url', "http://127.0.0.1:9890")
+    r = requests.post(orchestration_url + '/v1.0/alerts.json', data=json.dumps(data),headers=headers)
     
     return {}
 
